d404be1cd7239ccca1e709f2fa2fa368d5791b28
glocore <mailtoashwinm@gmail.com>
2023-03-03 19:19:45 +0530

    render basic file tree

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,34 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
+import tree from "./tree.json";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      {tree.map((item, index) => (
+        <div key={index}>{renderFileOrFolder(item as any, true)}</div>
+      ))}
     </div>
-  )
+  );
 }
 
-export default App
+type FileOrFolder =
+  | { type: "file"; name: string }
+  | { type: "directory"; name: string; contents: FileOrFolder[] };
+
+function renderFileOrFolder(fileOrFolder: FileOrFolder, root?: boolean) {
+  const paddingLeft = 25 * (root ? 0 : 1);
+
+  if (fileOrFolder.type === "file") {
+    return <div style={{ paddingLeft }}>{fileOrFolder.name}</div>;
+  } else {
+    return (
+      <div style={{ paddingLeft }}>
+        <span>{fileOrFolder.name}</span>
+        {fileOrFolder.contents.map((item, index) => (
+          <div key={index}>{renderFileOrFolder(item)}</div>
+        ))}
+      </div>
+    );
+  }
+}
+
+export default App;
